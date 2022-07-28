dbb8cd5a5260828a5705fd4d147a5c41e0945aee
yaahsin <brillanteoboe@gmail.com>
2022-07-28 21:05:15 +0800

    feat: add tweet modal and post route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const tweetController = require('../controllers/tweet-controller')
 
 router.get('/tweets', tweetController.getTweets)
+router.post('/tweets', tweetController.postTweet)
 
 router.use('/', (req, res) => res.redirect('/tweets'))
 
