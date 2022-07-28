cd9b1f38cc84730e61e154f8b959f53623b2f9ae
yaahsin <brillanteoboe@gmail.com>
2022-07-28 22:12:52 +0800

    feat: signupPage and modify post limit length

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const tweetController = require('../controllers/tweet-controller')
+const userController = require('../controllers/user-controller')
 
+router.get('/signup', userController.signUpPage)
 router.get('/tweets', tweetController.getTweets)
 router.post('/tweets', tweetController.postTweet)
 
