5121e803732e88b7cbca83edd348eb1ba336e192
yaahsin <brillanteoboe@gmail.com>
2022-08-01 13:45:56 +0800

    feat: link to post replies

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,15 @@
+const postPanel = document.querySelector('.table')
 
-// todo: modal顯示錯誤訊息
-document.getElementById('error').innerHTML = '內容不可空白'
+// 特定貼文詳情
+postPanel.addEventListener('click', e => {
+  console.log(e.target)
+  if (e.target.matches('.btn-show-reply')) {
+    console.log(e.target.dataset.id)
+  }
+})
+
+// wrapper.addEventListener('click', e => {()}
+// const tweet = document.getElementById('modal-description').value
+
+// document.getElementById('error').innerHTML = '請輸入推文內容!'
+// document.getElementById('error').innerHTML = '內容不可空白'
