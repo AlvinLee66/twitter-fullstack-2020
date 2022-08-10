f7d0609a3ba37bb02f21a47fc1b72498f773709a
AlvinLee66 <ewqaz001@gmail.com>
2022-08-10 16:10:23 +0800

    feat: A03 init

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,14 +1,11 @@
 const dayjs = require('dayjs')
 const relativeTime = require('dayjs/plugin/relativeTime')
 dayjs.extend(relativeTime)
-require('dayjs/locale/zh-tw')
-
-// 時區與本地同步
-const timezoneOffset = dayjs().utcOffset()
+// 時區不同，會少8小時
 
 module.exports = {
   currentYear: () => dayjs().year(),
-  relativeTimeFromNow: a => dayjs(a).locale('zh-tw').add(timezoneOffset).fromNow(),
+  relativeTimeFromNow: a => dayjs(a).hour(12).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
